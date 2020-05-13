c739446a7b6119cbb8cf689f34801c4bafa6fa2a
guling-shiyan <gulingshiyan@aliyun.com>
2020-05-13 10:04:33 +0800

    项目初始化

---
+++ src/main/java/com/study/dxbm/DxbmApplication.java
@@ -0,0 +1,13 @@
+package com.study.dxbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxbmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxbmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/dxbm/DxbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.dxbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxbmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
